93fa454cd5bd7fcdbcaf62641146a0414ddd9cba
Dominik Przychodni <dominik4658@gmail.com>
2025-03-06 16:44:03 +0000

    Added headers to gui (#12)
    * Added headers to gui
    
    * clippy
    
    * fmt
    
    * fmt, combo_box
    
    ---------
    
    Co-authored-by: Dominik Przychodni <dprzychodni@microsoft.com>

--- crates/crabapi/src/gui/iced/mod.rs
+++ crates/crabapi/src/gui/iced/mod.rs
@@ -1,6 +1,7 @@
 use iced;
-use iced::Element;
-use iced::widget::{Column, column, combo_box, text, text_input};
+use iced::widget::{Button, Column, Text, TextInput, column, row, text};
+use iced::widget::{button, combo_box, container};
+use iced::{Element, Length};
 
 use http::Method;
 
@@ -14,6 +15,11 @@ enum Message {
     UrlInputChanged(String),
     // HeaderInputChanged(String),
     // BodyInputChanged(String),
+    SendRequest,
+    HeaderKeyChanged(usize, String),
+    HeaderValueChanged(usize, String),
+    RemoveHeader(usize),
+    AddHeader,
 }
 
 #[derive(Debug)]
@@ -22,7 +28,7 @@ pub struct GUI {
     methods: combo_box::State<Method>,
     method_selected: Option<Method>,
     url_input: String, //http::Uri,
-                       // header_input: String, //http::HeaderMap,
+    header_input: Vec<(String, String)>, //http::HeaderMap,
                        // body_input: String,   //http::Body
 }
 
@@ -38,7 +44,7 @@ impl GUI {
             ]),
             method_selected: None,
             url_input: String::new(),
-            // header_input: String::new(),
+            header_input: vec![(String::new(), String::new())],
             // body_input: String::new(),
         }
     }
@@ -55,25 +61,77 @@ impl GUI {
             Message::UrlInputChanged(url) => {
                 self.url_input = url; //http::Uri::from_static("http://localhost:7878");
             } // Message::HeaderInputChanged(header) => {
-              //     self.header_input = header; // ttp::HeaderMap::new();
-              // }
-              // Message::BodyInputChanged(body) => {
-              //     self.body_input = body; // http::Body::from("Body");
-              // }
+            //     self.header_input = header; // ttp::HeaderMap::new();
+            // }
+            // Message::BodyInputChanged(body) => {
+            //     self.body_input = body; // http::Body::from("Body");
+            // }
+            Message::SendRequest => {
+                // TODO
+            }
+            Message::HeaderKeyChanged(index, key) => {
+                if let Some(header) = self.header_input.get_mut(index) {
+                    header.0 = key;
+                }
+            }
+            Message::HeaderValueChanged(index, value) => {
+                if let Some(header) = self.header_input.get_mut(index) {
+                    header.1 = value;
+                }
+            }
+            Message::AddHeader => {
+                self.header_input.push((String::new(), String::new()));
+            }
+            _ => {}
         }
     }
 
     fn view(&self) -> Element<Message> {
+        // ROW: Method, URI, Send Button
+        let url_input = TextInput::new("Enter URI", &self.url_input)
+            .on_input(Message::UrlInputChanged)
+            .width(Length::Fill);
+        let method_combo_box = combo_box(
+            &self.methods,
+            "Method",
+            self.method_selected.as_ref(),
+            Message::MethodChanged,
+        )
+        .width(75);
+        let send_button = Button::new(Text::new("Send").size(20)).on_press(Message::SendRequest);
+        let request_row = row![method_combo_box, url_input, send_button]
+            .spacing(10)
+            .padding(10)
+            .align_y(iced::Alignment::Center);
+
+        // ROW: Headers
+        let headers_title = Text::new("Headers").size(16);
+        let mut headers_column = column![headers_title].spacing(10);
+
+        for (i, header) in self.header_input.iter().enumerate() {
+            let header_row = row![
+                TextInput::new("Key", &header.0)
+                    .on_input(move |key| Message::HeaderKeyChanged(i, key))
+                    .width(Length::FillPortion(1)),
+                TextInput::new("Value", &header.1) // TODO: Change unwrap
+                    .on_input(move |value| Message::HeaderValueChanged(i, value))
+                    .width(Length::FillPortion(2)),
+                Button::new(Text::new("X"))
+                    .on_press(Message::RemoveHeader(i))
+                    .style(button::danger)
+            ]
+            .spacing(10);
+
+            headers_column = headers_column.push(header_row);
+        }
+
+        let add_header_button =
+            Button::new(Text::new("Add Header").size(20)).on_press(Message::AddHeader);
+        headers_column = headers_column.push(add_header_button);
+
         column![
-            GUI::label("Uri:"),
-            text_input("Uri", &self.url_input).on_input(Message::UrlInputChanged),
-            GUI::label("Method:"),
-            combo_box(
-                &self.methods,
-                "Method",
-                self.method_selected.as_ref(),
-                Message::MethodChanged
-            )
+            request_row,
+            container(headers_column).width(Length::Fill).padding(10)
         ]
         .into()
     }
